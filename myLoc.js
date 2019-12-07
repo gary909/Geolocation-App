900a62d3c79bcad19d6ecbc6fd6e73bdce0aab8d
Gary <gary909@gmail.com>
2019-12-07 14:46:54 +0100

    error status added

--- myLoc.js
+++ myLoc.js
@@ -2,7 +2,7 @@ window.onload = getMyLocation;
 
 function getMyLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(displayLocation);
+        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
     } else {
         alert("Oops, no geolocation support");
     }
@@ -14,4 +14,19 @@ function displayLocation(position) {
 
     var div = document.getElementById("location");
     div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+}
+
+function displayError(error) {
+    var errorTypes = {
+        0: "Unknown Error",
+        1: "Permission denied by user",
+        2: "Position is not available",
+        3: "Request timed out"
+    };
+    var errorMessage = errorTypes[error.code];
+    if (error.code == 0 || error.code == 2) {
+        errorMessage = errorMessage + " " + error.message;
+    }
+    var div = document.getElementById("location");
+    div.innerHTML = errorMessage;
 }
